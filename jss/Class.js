b56db41435bdf565a32f74f26ffdc4bfc5dc11ef
alfatlaloc <mr.alfatlaloc@gmail.com>
2019-04-01 10:04:15 -0600

    estilos

--- jss/Class.js
+++ jss/Class.js
@@ -40,6 +40,23 @@ const task = (function() {
     setStart_Day(n){
     	_Start_Day.set(this,n);
     }
+      
+      getEnd_Day(){
+    	return _End_Day.get(this);
+    }
+      
+    setEnd_Day(n){
+    	_End_Day.set(this,n);
+    }  
+
+            getID(){
+    	return _ID.get(this);
+    }
+      
+    setID(n){
+    	_ID.set(this,n);
+    }  
+      
   }
 return Task;
 }());
@@ -47,13 +64,12 @@ return Task;
 
 function New_Task(T_Name,T_Father,T_SD,T_ED,T_Id){
     let N_Task = new task(T_Name,T_Father,T_SD,T_ED,T_Id); //Constructor de nueva Tarea
-    Draw_Task(T_Name,T_Father,T_SD,T_ED,T_Id);      //Dibuja la tarea en el documento
-
+    Draw_Task(N_Task.getName(),N_Task.getStart_Day(),N_Task.getEnd_Day(),N_Task.getID());      //Dibuja la tarea en el documento
     console.log("El nombre: " + N_Task.getName());
 }
 
 
-function Draw_Task(T_Name,T_Father,T_SD,T_ED){
+function Draw_Task(T_Name,T_SD,T_ED){
     let art = document.createElement("article"); //Crea un articulo para la TASK y sus sub Task
     art.className="TASK"; //Clase TASK
     art.id=T_Name;    //Asigna el id de la tarea principal al articulo
@@ -61,9 +77,27 @@ function Draw_Task(T_Name,T_Father,T_SD,T_ED){
     let tnode = document.createTextNode(T_Name);
     titulo.appendChild(tnode);
     art.appendChild(titulo);
+    document.getElementById("TASK_area").appendChild(art);//Agrega el articulo a body
+    add_text_to_Draw_Task(art.id,T_SD);
+    art.appendChild(document.createElement("br"));
+    add_text_to_Draw_Task(art.id,T_ED);
     let t = document.createElement("progress"); //Barra de progreso
     t.max=100;     //Valor maximo 100%
-    t.value=0;      //Valor inicial a 0
-    art.appendChild(t);     
-    document.body.appendChild(art);//Agrega el articulo a body
+    t.value=25;      //Valor inicial a 0
+    art.appendChild(t);
+
 }
+
+
+function add_text_to_Draw_Task(art_id,text){
+    let texto=document.createElement("span");
+    let t = document.createTextNode(text);
+    texto.appendChild(t);
+    let art = document.getElementById(art_id);
+    art.appendChild(texto);
+}
+
+
+
+
+
